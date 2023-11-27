2878a2fdb28208c203f1013c86bb1253cd796267
timstr <straubinger.tim@gmail.com>
2023-11-26 22:32:21 -0800

    Fix AtomicSlice

--- src/core/atomicslice.rs
+++ src/core/atomicslice.rs
@@ -1,17 +1,73 @@
 use std::{
     cell::UnsafeCell,
-    ops::Deref,
+    ops::{BitXor, Deref, Not},
     sync::{
-        atomic::{AtomicBool, AtomicU16, Ordering},
+        atomic::{AtomicBool, AtomicUsize, Ordering},
         Arc,
     },
 };
 
+const CURRENT_SLICE_MASK: usize = 0x1;
+const CURRENT_SLICE_SHIFT: usize = 0;
+const NUM_SLICES: usize = 2; // bare minimum is 2. Consider making this larger and profiling
+
+const SLICE_1_MASK: usize = 0xFFFF_00;
+const SLICE_1_SHIFT: usize = 8;
+
+const SLICE_2_MASK: usize = 0xFFFF_0000_00;
+const SLICE_2_SHIFT: usize = 24;
+
+const SLICE_1_INC: usize = 1 << SLICE_1_SHIFT;
+const SLICE_2_INC: usize = 1 << SLICE_2_SHIFT;
+
+const INC_ALL_SLICES: usize = SLICE_1_INC | SLICE_2_INC;
+
+fn slice_1_use_count(status: usize) -> usize {
+    (status & SLICE_1_MASK) >> SLICE_1_SHIFT
+}
+
+fn slice_2_use_count(status: usize) -> usize {
+    (status & SLICE_2_MASK) >> SLICE_2_SHIFT
+}
+
+fn slice_use_count(slice: usize, status: usize) -> usize {
+    match slice {
+        0 => slice_1_use_count(status),
+        1 => slice_2_use_count(status),
+        _ => panic!("Invalid slice index"),
+    }
+}
+
+fn inc_slice(slice: usize) -> usize {
+    match slice {
+        0 => SLICE_1_INC,
+        1 => SLICE_2_INC,
+        _ => panic!("Invalid slice index"),
+    }
+}
+
+fn inc_all_slices_except(slice: usize) -> usize {
+    match slice {
+        0 => SLICE_2_INC,
+        1 => SLICE_1_INC,
+        _ => panic!("Invalid slice index"),
+    }
+}
+
+fn current_slice(status: usize) -> usize {
+    (status & CURRENT_SLICE_MASK) >> CURRENT_SLICE_SHIFT
+}
+
+fn valid_status(status: usize) -> bool {
+    let combined_mask = CURRENT_SLICE_MASK | SLICE_1_MASK | SLICE_2_MASK;
+    let leftover_bits = status & !combined_mask;
+    leftover_bits == 0
+}
+
 struct SharedData<T> {
     data: UnsafeCell<Vec<T>>,
     stride: usize,
-    slice_use_counts: Vec<AtomicU16>,
-    current_index: AtomicU16,
+    status: AtomicUsize,
     currently_writing: AtomicBool,
 }
 
@@ -21,19 +77,19 @@ pub(crate) struct AtomicSlice<T> {
 
 pub(crate) struct AtomicSliceReadGuard<'a, T> {
     slice: &'a [T],
-    use_count: &'a AtomicU16,
+    current_slice: usize,
+    status: &'a AtomicUsize,
 }
 
 impl<T: Default + Clone> AtomicSlice<T> {
     pub fn new(mut data: Vec<T>) -> AtomicSlice<T> {
         let stride = data.len();
-        let pool_size = 2; // bare minimum is 2. Consider making this larger and profiling
+        let pool_size = NUM_SLICES;
         data.resize(stride * pool_size, T::default());
         let shared_data = SharedData {
             data: UnsafeCell::new(data),
             stride,
-            slice_use_counts: (0..pool_size).map(|_| AtomicU16::new(0)).collect(),
-            current_index: AtomicU16::new(0),
+            status: AtomicUsize::new(0),
             currently_writing: AtomicBool::new(false),
         };
         AtomicSlice {
@@ -42,36 +98,53 @@ impl<T: Default + Clone> AtomicSlice<T> {
     }
 
     pub fn read<'a>(&'a self) -> AtomicSliceReadGuard<'a, T> {
-        let i = self.shared_data.current_index.load(Ordering::SeqCst) as usize;
-        let use_count = &self.shared_data.slice_use_counts[i];
-        // AHHHHHHHHHHHHHHHHH right here seems to be the mistake! If the writer updates the the current
-        // index and then checks the use count right here, it could see zero and proceed to write over
-        // the data without being synchronized properly.
-        // The writer must be prevented from observing this intermediate state, or this intermediate
-        // state must be removed or else made unproblematic.
-        // Ideally, getting the index and marking it as in use would be one and only one operation.
-        // How to do???
-        // Maybe some exotic trickery with low and high order bits?
-        // Actually, that seems fine, assuming that there are a smallish number of readers and the
-        // pool is smallish in size
-        //
-        // Consider:
-        // - pool size is always two
-        // - use count of both slices and which slice is active are all stored packed into one big atomic integer
-        // - when fetching the active slice, use a fetch_add to simultaneously increment use counts for *BOTH*
-        //   slices. Then as a second step, decrement the use count of the inactive slice
-        // - This way, the reader simultaneously retrieves and locks the active slice, and there is no longer
-        //   an intermediate step in which the current slice is loaded but not yet locked
-        use_count.fetch_add(1, Ordering::SeqCst);
+        // Get current slice index while also marking all slices as in use.
+        let status = self
+            .shared_data
+            .status
+            .fetch_add(INC_ALL_SLICES, Ordering::SeqCst);
+
+        debug_assert!(valid_status(status));
+        debug_assert!(slice_1_use_count(status) < 0xFFFF);
+        debug_assert!(slice_2_use_count(status) < 0xFFFF);
+
+        let current_slice = current_slice(status);
+
+        debug_assert!(
+            slice_use_count(
+                current_slice,
+                self.shared_data.status.load(Ordering::SeqCst)
+            ) > 0
+        );
+
+        // Now that the current slice is known, mark the others as no longer in use
+        let status = self
+            .shared_data
+            .status
+            .fetch_sub(inc_all_slices_except(current_slice), Ordering::SeqCst);
+        debug_assert!(valid_status(status));
+
         let stride = self.shared_data.stride;
-        let offset = i * stride;
+        let offset = current_slice * stride;
         let slice: &[T] = unsafe {
             let ptr_vec = self.shared_data.data.get();
             let ptr_data = (*ptr_vec).as_ptr();
             let ptr_begin = ptr_data.add(offset);
             std::slice::from_raw_parts(ptr_begin, stride)
         };
-        AtomicSliceReadGuard { slice, use_count }
+
+        debug_assert!(
+            slice_use_count(
+                current_slice,
+                self.shared_data.status.load(Ordering::SeqCst)
+            ) > 0
+        );
+
+        AtomicSliceReadGuard {
+            slice,
+            current_slice: current_slice,
+            status: &self.shared_data.status,
+        }
     }
 
     pub fn write(&self, data: &[T]) {
@@ -80,31 +153,35 @@ impl<T: Default + Clone> AtomicSlice<T> {
             panic!("Attempted to write slice of the wrong length to AtomicSlice");
         }
 
-        while self
+        // Wait for exclusive access to the write portion
+        while !self
             .shared_data
             .currently_writing
-            .swap(true, Ordering::SeqCst)
+            .compare_exchange(false, true, Ordering::SeqCst, Ordering::SeqCst)
+            .is_ok()
         {
             std::hint::spin_loop();
         }
-        // A useful precondition here would be that a slice exists which no current readers are accessing
-        // and which no future readers will access until after the atomic current index is written to to
-        // point to it. If that was given, then that slice would be the logical place to write new data
-        // to and assign the new current index to. To maintain that precondition for the next write,
-        // this needs to be a postcondition. So then, assuming that a completely unused slice exists,
-        // how to guarantee the existence of another one by the end of this function? In other words,
-        // how to guarantee by the end of this function that all readers are done with the old index (or
-        // some other index) and will always read the new index?
-        //
-        // Why do problems only appear to manifest when there is more than one writer?
-        let i = self.shared_data.current_index.load(Ordering::SeqCst) as usize;
-        let next_i = (i + 1) % self.shared_data.slice_use_counts.len();
-        // let prev_use_count = &self.shared_data.slice_use_counts[i];
-        // let next_use_count = &self.shared_data.slice_use_counts[next_i];
-        // while next_use_count.load(Ordering::SeqCst) != 0 {
-        //     std::hint::spin_loop();
-        // }
-        let offset = i * stride;
+
+        // Load the current status
+        let status = self.shared_data.status.load(Ordering::SeqCst);
+        debug_assert!(valid_status(status));
+        let i = current_slice(status);
+        debug_assert!(i < NUM_SLICES);
+        let next_i = (i + 1) % NUM_SLICES;
+
+        // Wait to ensure the next slice is not being used
+        loop {
+            let status = self.shared_data.status.load(Ordering::SeqCst);
+            debug_assert!(valid_status(status));
+            if slice_use_count(next_i, status) == 0 {
+                break;
+            }
+            std::hint::spin_loop();
+        }
+
+        // Copy data to the next slice
+        let offset = next_i * stride;
         let slice: &mut [T] = unsafe {
             let ptr_vec = self.shared_data.data.get();
             let ptr_data = (*ptr_vec).as_mut_ptr();
@@ -114,17 +191,19 @@ impl<T: Default + Clone> AtomicSlice<T> {
         for (i, v) in slice.iter_mut().enumerate() {
             *v = data[i].clone();
         }
-        self.shared_data
-            .current_index
-            .store(next_i as u16, Ordering::SeqCst);
 
-        // while prev_use_count.load(Ordering::SeqCst) != 0 {
-        //     std::hint::spin_loop();
-        // }
+        // Point all new readers to the next slice
+        let status = self
+            .shared_data
+            .status
+            .fetch_xor(i.bitxor(next_i), Ordering::SeqCst);
+        debug_assert!(valid_status(status));
 
+        // Release exclusive access to the write portion
         self.shared_data
             .currently_writing
-            .store(false, Ordering::SeqCst);
+            .compare_exchange(true, false, Ordering::SeqCst, Ordering::SeqCst)
+            .unwrap();
     }
 }
 
@@ -149,20 +228,31 @@ impl<'a, T> Deref for AtomicSliceReadGuard<'a, T> {
 
 impl<'a, T> Drop for AtomicSliceReadGuard<'a, T> {
     fn drop(&mut self) {
-        self.use_count.fetch_sub(1, Ordering::SeqCst);
+        let status = self
+            .status
+            .fetch_sub(inc_slice(self.current_slice), Ordering::SeqCst);
+        debug_assert!(valid_status(status));
+        debug_assert!(slice_use_count(self.current_slice, status) > 0);
     }
 }
 
 #[cfg(test)]
 mod test {
+    use std::sync::{
+        atomic::{AtomicU8, Ordering},
+        Arc,
+    };
+
     use super::AtomicSlice;
 
     #[test]
     fn test_atomic_slice_u8() {
         let length: usize = 16;
-        let num_readers = 4;
-        let num_writers = 4;
-        let num_iterations = 10_000_000;
+        let num_readers = 2;
+        let num_writers = 2;
+        let num_iterations = 1_000_000;
+
+        let next_value_to_write = Arc::new(AtomicU8::new(0));
 
         let mut data = Vec::<u8>::new();
         data.resize(length, 0);
@@ -172,7 +262,6 @@ mod test {
             .map(|i_reader| {
                 let atomic_slice = atomic_slice.clone();
                 std::thread::spawn(move || {
-                    println!("Reader {} starting", i_reader);
                     for iter in 0..num_iterations {
                         // Read the slice and assert that its length is as expected and that all values are the same
                         let guard = atomic_slice.read();
@@ -186,27 +275,26 @@ mod test {
                                 i_reader, first_value, other_value, iter, slice
                             );
                         }
-                        // println!("reader {}: all {}", i_reader, v0);
                     }
-                    println!("Reader {} done", i_reader);
                 })
             })
             .collect();
 
         let writers: Vec<std::thread::JoinHandle<()>> = (0..num_writers)
-            .map(|i_writer| {
+            .map(|_| {
                 let atomic_slice = atomic_slice.clone();
+                let next_value_to_write = Arc::clone(&next_value_to_write);
                 std::thread::spawn(move || {
-                    println!("Writer {} starting", i_writer);
                     let mut data = Vec::<u8>::new();
                     data.resize(length, 0);
                     for _ in 0..num_iterations {
-                        // Write an array of identical values to the slice
-                        data.fill(i_writer);
+                        // Write an array of identical values to the slice.
+                        // This should appear to be atomic, so no writer
+                        // should ever be able to observer a slice containing
+                        // unequal values
+                        data.fill(next_value_to_write.fetch_add(1, Ordering::Relaxed));
                         atomic_slice.write(&data);
-                        // println!("writer {} wrote all {}", i_writer, i_writer);
                     }
-                    println!("Writer {} done", i_writer);
                 })
             })
             .collect();
