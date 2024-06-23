634931854daec02ff55421e5d55383fa266aaea5
timstr <straubinger.tim@gmail.com>
2024-06-22 21:04:35 -0700

    Document expression graph data

--- src/core/expression/expressiongraphdata.rs
+++ src/core/expression/expressiongraphdata.rs
@@ -11,6 +11,8 @@ use super::{
     expressionnodeinput::ExpressionNodeInputId,
 };
 
+/// An expression node instance and its immediate topological
+/// information, intended to be stored in ExpressionGraphTopology.
 #[derive(Clone)]
 pub(crate) struct ExpressionNodeData {
     id: ExpressionNodeId,
@@ -19,6 +21,8 @@ pub(crate) struct ExpressionNodeData {
 }
 
 impl ExpressionNodeData {
+    /// Create a new ExpressionNodeData instance with the given id and
+    /// instance
     pub(crate) fn new(
         id: ExpressionNodeId,
         instance: Arc<dyn ExpressionNode>,
@@ -30,6 +34,11 @@ impl ExpressionNodeData {
         }
     }
 
+    /// Create a new ExpressionNodeData which does not yet contain an
+    /// instance, and is thus only partially valid. Attempts to access
+    /// the instance before it is supplied with `set_instance()` will
+    /// panic. This enable two-phase initialization, e.g. to make
+    /// safe topological changes in the instance's `new()` method.
     pub(crate) fn new_empty(id: ExpressionNodeId) -> ExpressionNodeData {
         ExpressionNodeData {
             id,
@@ -38,27 +47,33 @@ impl ExpressionNodeData {
         }
     }
 
+    /// Get the expression node's id
     pub(crate) fn id(&self) -> ExpressionNodeId {
         self.id
     }
 
+    /// Access the expression node instance
     pub(crate) fn instance(&self) -> &dyn ExpressionNode {
         self.instance.as_deref().unwrap()
     }
 
+    /// Access the expression node instance as an Arc
     pub(crate) fn instance_arc(&self) -> Arc<dyn ExpressionNode> {
         Arc::clone(self.instance.as_ref().unwrap())
     }
 
+    /// Set the instance, if self was created with `new_empty()`
     pub(crate) fn set_instance(&mut self, instance: Arc<dyn ExpressionNode>) {
         assert!(self.instance.is_none());
         self.instance = Some(instance);
     }
 
+    /// Access the list of input ids belonging to the expression node
     pub fn inputs(&self) -> &[ExpressionNodeInputId] {
         &self.inputs
     }
 
+    /// Mutably access the list of input ids belonging to the expression node
     pub fn inputs_mut(&mut self) -> &mut Vec<ExpressionNodeInputId> {
         &mut self.inputs
     }
@@ -76,10 +91,13 @@ impl Revision for ExpressionNodeData {
     }
 }
 
+/// The set of things that an expression node input or graph output
+/// can draw from, i.e. which produce a numeric value when evaluated.
 #[derive(Clone, Copy, Eq, PartialEq, Hash, Debug)]
 pub enum ExpressionTarget {
-    // TODO: Empty
+    /// The result of an expression node in the graph
     Node(ExpressionNodeId),
+    /// One of the supplied parameters to the graph
     Parameter(ExpressionGraphParameterId),
 }
 
@@ -95,12 +113,18 @@ impl From<ExpressionGraphParameterId> for ExpressionTarget {
     }
 }
 
+/// The set of things that a numeric value from either an
+/// expression node or a graph parameter can be sent to,
+/// e.g. things that require a numeric value in order to
+/// be evaluated.
 #[derive(Clone, Copy, Eq, PartialEq, Hash, Debug)]
 pub enum ExpressionDestination {
     NodeInput(ExpressionNodeInputId),
     Result(ExpressionGraphResultId),
 }
 
+/// The immediate topological data associated with the input of an
+/// expression node. Intended to be stored in ExpressionGraphTopology.
 #[derive(Clone)]
 pub(crate) struct ExpressionNodeInputData {
     id: ExpressionNodeInputId,
@@ -110,6 +134,7 @@ pub(crate) struct ExpressionNodeInputData {
 }
 
 impl ExpressionNodeInputData {
+    /// Create a new ExpressionNodeInputData instance
     pub(crate) fn new(
         id: ExpressionNodeInputId,
         owner: ExpressionNodeId,
@@ -123,27 +148,34 @@ impl ExpressionNodeInputData {
         }
     }
 
+    /// Get the input's id
     pub fn id(&self) -> ExpressionNodeInputId {
         self.id
     }
 
+    /// Get the input's target
     pub fn target(&self) -> Option<ExpressionTarget> {
         self.target
     }
 
+    /// Set the input's target
     pub fn set_target(&mut self, target: Option<ExpressionTarget>) {
         self.target = target;
     }
 
+    /// Get the input's owner, i.e. the expression node that it belongs to
     pub fn owner(&self) -> ExpressionNodeId {
         self.owner
     }
 
+    /// Get the input's default value, i.e. the value it produces when not
+    /// connected to anything
     pub fn default_value(&self) -> f32 {
         self.default_value
     }
 }
 
+/// Helper method for computing revision numbers with expression targets
 fn hash_optional_target(target: Option<ExpressionTarget>, hasher: &mut seahash::SeaHasher) {
     match target {
         Some(ExpressionTarget::Parameter(giid)) => {
@@ -171,6 +203,8 @@ impl Revision for ExpressionNodeInputData {
     }
 }
 
+/// The immediate topological data associated with one of the results of
+/// the expression graph.
 #[derive(Clone)]
 pub(crate) struct ExpressionGraphResultData {
     id: ExpressionGraphResultId,
@@ -179,6 +213,7 @@ pub(crate) struct ExpressionGraphResultData {
 }
 
 impl ExpressionGraphResultData {
+    /// Create a new ExpressionGraphResultData instance.
     pub(crate) fn new(
         id: ExpressionGraphResultId,
         default_value: f32,
@@ -190,18 +225,23 @@ impl ExpressionGraphResultData {
         }
     }
 
+    /// Get the graph result's id
     pub fn id(&self) -> ExpressionGraphResultId {
         self.id
     }
 
+    /// Get the graph result's target
     pub fn target(&self) -> Option<ExpressionTarget> {
         self.target
     }
 
+    /// Set the graph result's target
     pub fn set_target(&mut self, target: Option<ExpressionTarget>) {
         self.target = target;
     }
 
+    /// Get the result's default value, i.e. the value it produces when not
+    /// connected to anything
     pub fn default_value(&self) -> f32 {
         self.default_value
     }
