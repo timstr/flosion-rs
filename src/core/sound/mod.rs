b251fac004ce62f59bcb060df2376a84f250a3e4
timstr <straubinger.tim@gmail.com>
2024-09-07 20:39:29 -0700

    Delete ancient serialization code

--- src/core/sound/mod.rs
+++ src/core/sound/mod.rs
@@ -1,12 +1,11 @@
 pub mod context;
-pub mod soundgraphid;
-// pub mod graphserialization;
 pub mod expression;
 pub mod expressionargument;
 mod path;
 pub mod sounderror;
 pub mod soundgraph;
 pub(crate) mod soundgraphdata;
+pub mod soundgraphid;
 pub mod soundgraphproperties;
 pub(crate) mod soundgraphvalidation;
 pub mod soundinput;
--- src/core/sound/test/mod.rs
+++ src/core/sound/test/mod.rs
@@ -1,3 +1,2 @@
-// mod graphserializationtest;
 mod soundgraphvalidationtest;
 mod testobjects;
